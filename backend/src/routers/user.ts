95fab22de8b69dcda89fdf7acde8bf1795ce1cd4
Mrityunjay <mjguptacse@gmail.com>
2024-11-19 21:54:20 +0530

    Task Submission API Done

--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -108,7 +108,7 @@ router.post("/task", authMiddleware, async (req, res) => {
     const response = await tx.task.create({
       data: {
         title: parseData.data.title ?? DEFAULT_TITLE,
-        amount: 1,
+        amount: 1 * TOTAL_DECIMALS,
         signature: parseData.data.signature,
         user_id: userId,
       },
--- backend/src/routers/worker.ts
+++ backend/src/routers/worker.ts
@@ -11,6 +11,7 @@ import { createSubmissionInput } from "../types";
 const prismaClient = new PrismaClient();
 
 const router = Router();
+const TOTAL_SUBMISSIONS = 100;
 
 router.post("/signin", async (req, res) => {
   const hardCoded = "adfcsbcd";
@@ -68,5 +69,72 @@ router.get("/nextTask", workerMiddleware, async (req, res) => {
       task,
     });
   }
+});
+
+router.post("/submission", workerMiddleware, async (req, res) => {
+  // @ts-ignore
+  const userId = req.userId;
+  const body = req.body;
+  const parsedBody = createSubmissionInput.safeParse(body);
+
+  if (parsedBody.success) {
+    const task = await getNextTask(Number(userId));
+    if (!task || task?.id !== Number(parsedBody.data.taskId)) {
+      return res.status(411).json({
+        message: "Incorrect task id",
+      });
+    }
+
+    const amount = (Number(task.amount) / TOTAL_SUBMISSIONS).toString();
+
+    const submission = await prismaClient.$transaction(async (tx) => {
+      const submission = await tx.submission.create({
+        data: {
+          option_id: Number(parsedBody.data.selection),
+          worker_id: userId,
+          task_id: Number(parsedBody.data.taskId),
+          amount: Number(amount),
+        },
+      });
+
+      await tx.worker.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          pending_amount: {
+            increment: Number(amount),
+          },
+        },
+      });
+
+      return submission;
+    });
+
+    const nextTask = await getNextTask(Number(userId));
+    res.json({
+      nextTask,
+      amount,
+    });
+  } else {
+    res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+});
+router.get("/balance", workerMiddleware, async (req, res) => {
+  // @ts-ignore
+  const userId: string = req.userId;
+
+  const worker = await prismaClient.worker.findFirst({
+    where: {
+      id: Number(userId),
+    },
+  });
+
+  res.json({
+    pendingAmount: worker?.pending_amount,
+    lockedAmount: worker?.pending_amount,
+  });
 });
 export default router;
